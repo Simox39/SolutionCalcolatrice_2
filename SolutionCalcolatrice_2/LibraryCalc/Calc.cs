0a32efe25ade05387685eeb46d317950afcc5d48
Simone Cocchi <ilgrosso7516@gmail.com>
2018-02-13 13:19:32 +0100

    Ho fatto una seconda modifica

--- SolutionCalcolatrice_2/LibraryCalc/Calc.cs
+++ SolutionCalcolatrice_2/LibraryCalc/Calc.cs
@@ -31,25 +31,82 @@ namespace LibraryCalc
 
   
    
-       { public static double Massimo(double a, double b)
+        public static double Massimo(double a, double b)
+        {
+         double ris = 0;
+         if (a > b)
+            ris = a;
+         else
+            ris = b;
+
+         return ris;
+        }
+    
+         public static double Minimo(double a, double b)
+         {
+          double ris = 0;
+           if (a < b)
+            ris = a;
+           else
+            ris = b;
+
+          return ris;
+         }
+       public static double Media(double a, double b)
+       {
+        double ris = 0;
+        ris = (a + b) / 2;
+
+        return ris;
+       }
+    
+    
+        public static double MediaTreNumeri(double a, double b, double c)
         {
             double ris = 0;
-        if(a > b)
-        {
-
-        }if(a < b)
-        {
-
-        }
-        if(a==b)
-        {
-            ris = a;
-            ris = b;
-        }
-            
+            ris = (a + b + c) / 3;
             return ris;
         }
+
+    public static double MassimoTreNumeri(double a, double b, double c)
+    {
+        double ris = 0;
+        if (a > b && a > c)
+            ris = a;
+        else
+        {
+            ris = b;
+        }
+           
+        else
+        {
+            ris = c;
+        }
+       
+
+        return ris;
     }
+
+    public static double MinimoTreNumeri(double a, double b, double c)
+    {
+        double ris = 0;
+        if (a < b && a < c)
+            ris = a;
+        else
+        {
+            ris = b;
+        }
+           
+        else
+        {
+            ris = c;
+        }
+
+
+        return ris;
+    }
+    
+} 
  
 
 
